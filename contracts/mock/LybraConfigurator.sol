a2002eca69833eb2899e4b88bd00d5d2eb320935
LidoKing <55234791+LidoKing@users.noreply.github.com>
2023-09-20 09:47:33 +0800

    chore: added eusd getter to configurator

--- contracts/mock/LybraConfigurator.sol
+++ contracts/mock/LybraConfigurator.sol
@@ -4,14 +4,27 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LybraConfigurator is Ownable {
+    address EUSD;
     mapping(address => uint256) vaultWeight;
+
+    constructor (address _eusd) {
+        EUSD = _eusd;
+    }
 
     function setVaultWeight(address pool, uint256 weight) external onlyOwner {
         vaultWeight[pool] = weight;
     }
 
+    function setEUSD(address _eusd) external onlyOwner {
+        EUSD = _eusd;
+    }
+
     function getVaultWeight(address pool) external view returns (uint256) {
         if (vaultWeight[pool] == 0) return 100 * 1e18;
         return vaultWeight[pool];
+    }
+
+    function getEUSDAddress() external view returns (address) {
+        return EUSD;
     }
 }
