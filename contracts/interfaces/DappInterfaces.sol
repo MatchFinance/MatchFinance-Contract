ad65de14756c122e54654a4aa0d42cdeb9107212
LidoKing <55234791+LidoKing@users.noreply.github.com>
2023-11-07 14:25:37 +0800

    Revert "mesLBR & staking draft"

--- contracts/interfaces/DappInterfaces.sol
+++ contracts/interfaces/DappInterfaces.sol
@@ -6,26 +6,6 @@ interface ILido {
     function submit(address _referral) external payable returns (uint256 StETH);
 }
 
-interface IWstETH {
-    function wrap(uint256 _stETHAmount) external returns (uint256);
-}
-
-interface IWBETH {
-    function deposit(address referral) external payable;
-}
-
-interface IRocketDepositPool {
-    function deposit() external payable;
-}
-
-interface IRocketStorageInterface {
-    function getAddress(bytes32 _key) external view returns (address);
-}
-
-interface IDepositHelper {
-    function toLSD() external payable returns (uint256);
-}
-
 interface IUniswapV2Router {
     function swapExactETHForTokens(
         uint amountOutMin, 
